f0f9cf3376570bc9dc40c52b288918e5a24b6ce5
Lukash <lu_kash10@hotmail.com>
2023-12-30 03:05:25 -0300

    Icon

--- src/layouts/default/DefaultNav.vue
+++ src/layouts/default/DefaultNav.vue
@@ -13,7 +13,7 @@
             <button class="btn" style="background-color:white;border: none;">
               <router-link to="/" style="color:black;text-decoration: none !important;">
                 <a>
-                  <i class="fa-solid fa-home background-absolem"></i> <b>Início</b>
+                  <i class="fa-solid fa-home" style="color:#5b257f"></i> <b>Início</b>
                 </a>
               </router-link>
             </button>
